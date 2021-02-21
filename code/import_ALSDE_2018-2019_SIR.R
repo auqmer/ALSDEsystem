df9181cc7584ee0aceae2199b3163e933ce4dc45
wmmurrah <william.murrah@gmail.com>
2021-02-21 11:46:36 -0600

    Add import ALSDA_2018-2019_SIR stub

---
+++ code/import_ALSDE_2018-2019_SIR.R
@@ -0,0 +1,12 @@
+#************************************************************************
+# Title: import_ALSDE_2018-2019_SIR.R
+# Author: William Murrah
+# Description: description
+# Created: Sunday, 21 February 2021
+# R version: R version 4.0.4 (2021-02-15)
+# Project(working) directory: /Users/wmm0017/Projects/QMER/ALSDEsystem
+#************************************************************************
+
+library(readxl)
+
+sir1819 <- read_xlsx("")
